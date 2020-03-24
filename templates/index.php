11a4fb6d89fa7946c17db7c2c51d857f2984c979
Ilin-Anton <antonilin1997@mail.ru>
2020-03-24 20:22:57 +0300

    4,5 Task

--- templates/index.php
+++ templates/index.php
@@ -1,15 +1,47 @@
-<?php
-require_content('functions.php');
-$page_content=compile_template('index.php',[
-'category_list'=>$Cat;
-'Lots_list'=>$Lots;
-])
-$layout_content=compile_template('layuot.php',[
-'page_title'=>'Главная',
-'is_auth'=>$is_auth,
-'user_name'=>$user_name,
-'page_content'=>$page_content,
-'category_list'=>$Cat
-]);
-print($layout_content);
-?>
+<section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+            <?php
+            foreach ($Cat as $key => $value) {
+            ?>
+            <li class="promo__item promo__item--<?=$key?>">
+            <a class="promo__link" href="pages/all-lots.html"><?=$value?></a>
+            </li>
+            <?php
+            }
+            ?>
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+            <?php
+            foreach ($Lots as $lot) {
+            ?>
+            <li class="lots__item lot">
+            <div class="lot__image">
+            <img src="<?=$lot["D"]?>" width="350" height="260" alt="">
+            </div>
+            <div class="lot__info">
+            <span class="lot__category"><?=$lot["B"]?></span>
+            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot["A"]?></a></h3>
+            <div class="lot__state">
+            <div class="lot__rate">
+            <span class="lot__amount">Стартовая цена <?=format_number($lot["C"],false)?></span>
+            <span class="lot__cost"><?=format_number($lot["C"],true)?></span>
+            </div>
+            <div class="lot__timer timer">
+            <?=LTime()?>
+            </div>
+            </div>
+            </div>
+            </li>
+            <?php
+            }
+            ?>
+            
+        </ul>
+    </section>
--- templates/layout.php
+++ templates/layout.php
@@ -1,29 +1,3 @@
-<?php
-$is_auth = rand(0, 1);
-
-$user_name = 'Anton'; // укажите здесь ваше имя
-
-$Cat=['boards'=>'Доски и лыжи', 'attachment'=>'Крепления', 'boots'=>'Ботинки', 'clothing'=>'Одежда', 'tools'=>'Инструменты', 'other'=>'Разное'];
-$Lots=[
-['A'=>'2014 Rossignol District Snowboard', 'B'=>'Доски и лыжи', 'C'=>10999, 'D'=>'img/lot-1.jpg'],
-['A'=>'DC Ply Mens 2016/2017 Snowboard', 'B'=>'Доски и лыжи', 'C'=>159999, 'D'=>'img/lot-2.jpg'],
-['A'=>'Крепления Union Contact Pro 2015 года размер L/XL', 'B'=>'Крепления', 'C'=>8000, 'D'=>'img/lot-3.jpg'],
-['A'=>'Ботинки для сноуборда DC Mutiny Charocal', 'B'=>'Ботинки', 'C'=>10999, 'D'=>'img/lot-4.jpg'],
-['A'=>'Куртка для сноуборда DC Mutiny Charocal', 'B'=>'Одежда', 'C'=>7500, 'D'=>'img/lot-5.jpg'],
-['A'=>'Маска Oakley Canopy', 'B'=>'Разное', 'C'=>5400, 'D'=>'img/lot-6.jpg'],
-];
-function format_number($number, $rub){
-$number=ceil($number);
-$number=number_format($number, 0, ' ', ' ');
-return $rub?$number."<b class='rub'>р</b>": $number;
-if ($number>=1000){
-$C=number_format($number, 0, ' ', ' ');
-}
-else{
-$C=$number;
-}
-}
-?>
 <!DOCTYPE html
 <html lang="ru">
 <head>
